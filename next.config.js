66f5734f3efa71cbafe64e808234498cbace09d6
Thy Sok <thysok@gmail.com>
2021-05-25 09:24:13 -0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    images: {
+        domains: [
+            "cdn0.iconfinder.com",
+            "links.papareact.com",
+            "platform-lookaside.fbsbx.com",
+            "firebasestorage.googleapis.com",
+        ],
+    },
+};
